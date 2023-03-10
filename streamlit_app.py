3241cadbd615daa2acc548ef269bbc899493396b
malavikathiruchelvam <55451229+malavikathiruchelvam@users.noreply.github.com>
2023-03-10 22:56:32 +0530

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment -what does the next line do? 
@@ -27,6 +28,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+ fruit_choice")
 streamlit.text(fruityvice_response)
